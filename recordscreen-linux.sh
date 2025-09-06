d0d853bef8240854d1cb4a923ebb4b5acfa8dc03
Chen Gang <gchen@itskywalker.com>
2025-09-07 06:55:22 +0800

    Add the commands to record screen and input microphone under linux
    Signed-off-by: Chen Gang <gchen@itskywalker.com>

---
+++ recordscreen-linux.sh
@@ -0,0 +1,7 @@
+
+apt install -y ffmpeg oss-compat alsa-oss
+apt install mencoder
+
+# in gui user account:
+aoss ffmpeg -f oss -i /dev/dsp -f x11grab -s wxga -r 25 -i :0.0  /tmp/out5.mpg
+mencoder  /tmp/out5.mpg -o /tmp/m001.mp4 -oac mp3lame -ovc x264 -of lavf -vf lavcdeint
