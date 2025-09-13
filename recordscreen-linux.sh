8a92c235de5a42b0f4cfb7aa0a645f9afcc94ff0
Chen Gang <gchen@itskywalker.com>
2025-09-13 20:53:44 +0800

    Update the demos, toolchains and recordscreen
    Signed-off-by: Chen Gang <gchen@itskywalker.com>

--- recordscreen-linux.sh
+++ recordscreen-linux.sh
@@ -5,3 +5,4 @@ apt install mencoder
 # in gui user account:
 aoss ffmpeg -f oss -i /dev/dsp -f x11grab -s wxga -r 25 -i :0.0  /tmp/out5.mpg
 mencoder  /tmp/out5.mpg -o /tmp/m001.mp4 -oac mp3lame -ovc x264 -of lavf -vf lavcdeint
+adb shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file:///sdcard/Movies/python.mp4
