bec6e0d6ed597074aeb3d734fcd87b16e89ecf87
Chen Gang <gchen@itskywalker.com>
2025-10-09 23:09:01 +0800

    增加程序的解释模拟的综合示例视频，以及基本的提纲
    Signed-off-by: Chen Gang <gchen@itskywalker.com>

--- recordscreen-linux.sh
+++ recordscreen-linux.sh
@@ -3,6 +3,18 @@ apt install -y ffmpeg oss-compat alsa-oss
 apt install mencoder
 
 # in gui user account:
-aoss ffmpeg -f oss -i /dev/dsp -f x11grab -s wxga -r 25 -i :0.0  /tmp/out5.mpg
+#aoss ffmpeg -f oss -i /dev/dsp -f x11grab -s wxga -r 25 -i :0.0  /tmp/out5.mpg
+ffmpeg -f alsa -i plughw:0,0 -f x11grab -s wxga -r 25 -i :0.0  /tmp/out5.mpg
 mencoder  /tmp/out5.mpg -o /tmp/m001.mp4 -oac mp3lame -ovc x264 -of lavf -vf lavcdeint
 adb shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file:///sdcard/Movies/python.mp4
+
+
+gchen@clid12:~$ arecord -l
+**** List of CAPTURE Hardware Devices ****
+card 0: Device [USB PnP Audio Device], device 0: USB Audio [USB Audio]
+  Subdevices: 1/1
+  Subdevice #0: subdevice #0
+card 1: PCH [HDA Intel PCH], device 0: ALC256 Analog [ALC256 Analog]
+  Subdevices: 1/1
+  Subdevice #0: subdevice #0
+gchen@clid12:~$ 
---
+++ toolchains/program-xxx/program-interpreter-emulator/README.md
@@ -0,0 +1,21 @@
+# 程序的解释模拟
+
+## 功能视角：
+
+1. 定位：高级语言/异构计算/环境硬件
+2. 产品：Pyton/Java/JS/Shell，AI(Mesa/Cuda/昇腾)，Qemu/Wine
+3. 学习：实践(Linux)/理论(科普)/AI(DeepSeek/豆包)
+
+## 使用视角：
+
+1. 普通：标准(Java/Python/JS/Shell)/JIT/JOT/跨语言调用
+2. 异构：驱动固件/图形(Mesa)/AI(Cuda/昇腾)
+3. 环硬：Qemu/Wine/库模拟(Hudini)
+
+## 开发视角：
+
+1. 普通：驱动固件/分发(ELF/OM)/内存映射
+2. 处理：转译(LLVM/Qemu)/跨语言(JNI/PNI)/硬件(CPU/内存/其他设备直接用)
+3. 环境：内存布局/进程启动加载/线程对应关系/内核态交互(系统调用/信号处理/内核文件系统)
+
+## 综合示例：qemu_static示例
