fd2a83731f1573097a1c90e4e734c75662370c72
Chen Gang <chengang_19780320@qq.com>
2026-07-30 23:17:18 +0800

    map: Sync all map latest contents
    Signed-off-by: Chen Gang <chengang_19780320@qq.com>

--- recordscreen-linux.sh
+++ recordscreen-linux.sh
@@ -1,11 +1,24 @@
 
+#
+# For wayland
+#
+rm -rf /tmp/temp_lossless.mov; clear; wf-recorder -a alsa_input.platform-snd-card_1.stereo-fallback -c qtrle -r 25 -f /tmp/temp_lossless.mov > /tmp/info.log 2>&1
+rm -rf /tmp/out4.mp4; ffmpeg -y -i /tmp/temp_lossless.mov -c:v libx264 -preset veryfast -crf 26 -pix_fmt yuv420p -c:a aac -b:a 128k -af "volume=24dB" /tmp/out4.mp4
+adb push /tmp/out4.mp4 /sdcard/Movies/
+adb shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file:///sdcard/Movies/out4.mp4
+
+#
+# For x11
+#
+
 apt install -y ffmpeg oss-compat alsa-oss
 apt install mencoder
 
 # in gui user account:
 #aoss ffmpeg -f oss -i /dev/dsp -f x11grab -s wxga -r 25 -i :0.0  /tmp/out5.mpg
 #ffmpeg -f alsa -i plughw:1,0 -f x11grab -s wxga -r 25 -i :0.0  /tmp/out5.mpg
-clear; rm -rf /tmp/out5.mpg; ffmpeg -f alsa -i plughw:1,0 -f x11grab -s wxga -r 25 -i :0.0  /tmp/out5.mpg > /tmp/info.log 2>&1
+#clear; rm -rf /tmp/out5.mpg; ffmpeg -f alsa -i plughw:1,0 -f x11grab -s wxga -r 25 -i :0.0  /tmp/out5.mpg > /tmp/info.log 2>&1
+clear; rm -rf /tmp/out5.mpg; ffmpeg -f alsa -i plughw:0,0 -f x11grab -s wxga -r 25 -i :0.0  /tmp/out5.mpg > /tmp/info.log 2>&1
 mencoder  /tmp/out5.mpg -o /tmp/m001.mp4 -oac mp3lame -ovc x264 -of lavf -vf lavcdeint
 adb shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file:///sdcard/Movies/python.mp4
 
